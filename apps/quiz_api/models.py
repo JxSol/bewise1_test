84f6b52f42722ea7a5971f45d5131c82bab21ea0
JxSol <jxsol.js@gmail.com>
2023-05-24 13:37:30 +0600

    add serializer for model

--- apps/quiz_api/models.py
+++ apps/quiz_api/models.py
@@ -4,12 +4,23 @@ from apps import db
 
 
 class Question(db.Model):
+    """Question model.
+    Attrs:
+        id (Integer): identifier
+        question (String): text of the question
+        answer (String): text of the answer
+        created (DateTime): time when the record was created
+    """
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255), nullable=False, unique=True)
     answer = db.Column(db.String(255), nullable=False)
     created = db.Column(db.DateTime, default=datetime.now)
 
-    def __repr__(self):
+    def serialize(self) -> dict:
+        """Serialize the object."""
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self) -> str:
         return str(self.question)
 
 
